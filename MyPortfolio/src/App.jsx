49c7b0436d3f7c623c9b274a88f02d87dd4b9d63
Makayla Wray <mwray1@my.westga.edu>
2024-08-22 22:02:40 -0400

    Make Nav Bar Responsive

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,7 +1,10 @@
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { NavBar } from './components/Navbar/Navbar';
 
 function App() {
-  return <div className={styles.App}> Hello World</div>;
+  return <div className={styles.App}>
+    <NavBar/>
+  </div>;
   
 }
 
